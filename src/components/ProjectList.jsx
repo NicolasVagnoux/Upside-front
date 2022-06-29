3a8eae0bb1ffa366a4abd47489cf489812d4b027
Nicolas Vagnoux <nicolas.vagnoux@gmail.com>
2022-06-29 21:56:37 +0200

    first axios for projectcards

---
+++ src/components/ProjectList.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ProjectCard from "./ProjectCard";
+
+const ProjectList = () => {
+  const [projectList, setProjectList] = useState([]);
+
+  useEffect(() => {
+    const getProjectList = async () => {
+      const { data } = await axios.get(`http://localhost:3000/api/projects`);
+      setProjectList(data);
+    };
+    getProjectList();
+  }, []);
+  console.log(projectList);
+
+  return (
+    <div>
+      {projectList &&
+        projectList.map((project, index) => (
+          <ProjectCard key={index} {...project} />
+        ))}
+    </div>
+  );
+};
+
+export default ProjectList;

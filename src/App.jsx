2ef67680e9ac9bbd9b483b27104f2b9ae00d478a
Nicolas Vagnoux <nicolas.vagnoux@gmail.com>
2022-06-30 12:22:47 +0200

    created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 import User from "./components/User";
 
 function App() {
   return (
     <div>
-      <Home />
-      <User />
+      <Navbar />
+      <Routes>
+        <Route path="*" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/user" element={<User />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import navbar from "../../data/navbar";
+
+const Navbar = () => {
+  return (
+    // Navbar complète
+    <div className="fixed py-4 h-full w-[80px] bg-[#0068FF] flex flex-col justify-between items-center">
+      <img
+        className="w-[50px] logo-shadow"
+        src="./assets/upside.png"
+        alt="upside"
+      />
+      {/* Liste */}
+      <ul className="flex flex-col items-center justify-center gap-8">
+        {navbar &&
+          navbar.map((item) => (
+            <Link to={item.link}>
+              <img className="w-[25px] logo-shadow" src={item.src} alt="img" />
+            </Link>
+          ))}
+      </ul>
+        <img
+          className="w-[30px] logo-shadow cursor-pointer"
+          src="./assets/logout.svg"
+          alt="logout"
+        />
+    </div>
+  );
+};
+
+export default Navbar;

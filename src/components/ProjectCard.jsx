f2bf80eaa651eba80ce00e97e17c33542fda0290
JohnnyHiriart <johnnyhc@hotmail.com>
2022-06-30 02:42:01 +0200

    modif BDD

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const ProjectCard = ({ nameProject, projectDesc, client }) => {
+const ProjectCard = ({ nameProject, projectDesc, client,subsidiary,startDate, finalDate, progress, industryTag, color, projectManager }) => {
   return (
     <div className="border-2 border-black w-[400px] h-[200px] rounded-lg flex flex-col justify-around items-center">
       <h1 className="text-xl font-bold">{nameProject}</h1>
       <p>{projectDesc}</p>
       <p>{client}</p>
+      <p>{subsidiary}</p>
+      <p>{startDate.slice(0,10).split("-").reverse().join("/")}</p>
+      <p>{finalDate.slice(0,10).split("-").reverse().join("/")}</p>
+      <p>{progress}</p>
+      <p>{industryTag}</p>
+      <p>{color}</p>
+      <p>{projectManager}</p>
     </div>
   );
 };

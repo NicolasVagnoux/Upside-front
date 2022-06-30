002559b9f4e14a4b23af69376d50d33e58256226
AntsoTech <antso-rakotoson@hotmail.fr>
2022-06-30 04:55:15 +0200

    added days left

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,18 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const ProjectCard = ({ nameProject, projectDesc, client,subsidiary,startDate, finalDate, progress, industryTag, color, projectManager }) => {
+const ProjectCard = ({
+  nameProject,
+  projectDesc,
+  client,
+  subsidiary,
+  startDate,
+  finalDate,
+  progress,
+  industryTag,
+  color,
+  projectManager,
+}) => {
   return (
-    <div className="border-2 border-black w-[400px] h-[200px] rounded-lg flex flex-col justify-around items-center">
+    <div
+      className={`border-2 w-[300px] h-[270px] rounded-lg flex flex-col justify-around items-center bg-[${color}]`}
+    >
       <h1 className="text-xl font-bold">{nameProject}</h1>
       <p>{projectDesc}</p>
       <p>{client}</p>
       <p>{subsidiary}</p>
-      <p>{startDate.slice(0,10).split("-").reverse().join("/")}</p>
-      <p>{finalDate.slice(0,10).split("-").reverse().join("/")}</p>
+      <p>{startDate.slice(0, 10).split("-").reverse().join("/")}</p>
+      <p>{finalDate.slice(0, 10).split("-").reverse().join("/")}</p>
+      <p>
+        {Math.abs(
+          new Date(finalDate).getTime() - new Date(startDate).getTime()
+        ) /
+          (1000 * 3600 * 24)}{" "}
+        days left
+      </p>
       <p>{progress}</p>
       <p>{industryTag}</p>
-      <p>{color}</p>
       <p>{projectManager}</p>
     </div>
   );
